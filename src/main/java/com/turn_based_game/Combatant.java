915104b9b274d0ac16dbe479a11d4fce0bdbfa31
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-03-02 11:30:11 -0500

    filled out getters and behaviors on combatant

--- src/main/java/com/turn_based_game/Combatant.java
+++ src/main/java/com/turn_based_game/Combatant.java
@@ -13,29 +13,82 @@ public abstract class Combatant {
     private int currentHealth;
     private int maxDefence;
     private int currentDefence;
-    private List<Action> actionsAvailable;
+    private List<Action> actions;
 
     // Getters and Setters
-    // TODO: add Getters and Setters (See repo issue #10 for what's needed.)
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public int getMaxDefence() {
+        return maxDefence;
+    }
+
+    public int getCurrentDefence() {
+        return currentDefence;
+    }
+
+    public List<Action> getActions() {
+        return actions;
+    }
+
+    public boolean isDefeated() {
+        return currentHealth <= 0;
+    }
+
+    public boolean isPlayer() {
+        return false;
+    }
 
     // Constructors
-    public Combatant(String name, int maxHealth, int maxDefence, Action[] actionsAvailable) {
+    public Combatant(String name, int maxHealth, int maxDefence, Action[] actions) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
         this.maxDefence = maxDefence;
         this.currentDefence = maxDefence;
-        this.actionsAvailable = Arrays.asList(actionsAvailable);
+        this.actions = Arrays.asList(actions);
     }
 
     // Behaviors
-    public void increaseHealth(int hp) {}
+    public void increaseHealth(int amount) {
+        currentHealth += amount;
+        if(currentHealth > maxHealth){
+            currentHealth = maxHealth;
+        }
+    }
 
-    public void decreaseHealth(int hp) {}
+    public void decreaseHealth(int amount) {
+        currentHealth -= amount;
+    }
 
-    public void increaseDefense(int amount) {}
+    public void increaseDefense(int amount) {
+        currentDefence += amount;
+        if(currentDefence > maxDefence){
+            currentDefence = maxDefence;
+        }
+    }
 
-    public void decreaseDefense(int amount) {}
+    public void decreaseDefense(int amount) {
+        currentDefence -= amount;
+        if(currentDefence < 0){
+            currentDefence = 0;
+        }
+    }
 
-    public void doAction(Action actionToDo) {}
+    public void addAction(Action newAction) {
+        actions.add(newAction);
+    }
+
+    public void removeAction(Action actionToDelete) {
+        actions.remove(actionToDelete);
+    }
 }

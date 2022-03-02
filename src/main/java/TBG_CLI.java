d61c12842493f72c9202657550319dcb0bec0c3d
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-03-02 11:29:23 -0500

    renamed application

---
+++ src/main/java/TBG_CLI.java
@@ -0,0 +1,10 @@
+import com.turn_based_game.BattleManager;
+
+public class TBG_CLI {
+
+    public static void main(String[] args) {
+        BattleManager battleManager = new BattleManager();
+        System.out.println("Hello World!");
+    }
+
+}

5c72e00641ed80ad02c10e88a3c9c195cc652ad9
Muggyfox1 <47337746+Muggyfox1@users.noreply.github.com>
2022-03-04 10:30:33 -0500

    refactored BattleManager
    Refactored Battle Manager to be more of a controller rather than a View.

--- src/main/java/TBG_CLI.java
+++ src/main/java/TBG_CLI.java
@@ -1,9 +1,28 @@
 import com.turn_based_game.BattleManager;
+import com.turn_based_game.Combatant;
+import com.turn_based_game.Enemy;
+import com.turn_based_game.Player;
+import com.turn_based_game.actions.Action;
+import com.turn_based_game.actions.BasicAttack;
+import com.turn_based_game.model.Battle;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TBG_CLI {
 
     public static void main(String[] args) {
-        BattleManager battleManager = new BattleManager();
+        List<Combatant> combatants = new ArrayList<>();
+
+        Action[] actions = new Action[]{new BasicAttack()};
+        Combatant player = new Player("Programmer", 10, 5, actions);
+        Combatant bug = new Enemy("Syntax error", 5, 5, actions);
+
+        combatants.add(player);
+        combatants.add(bug);
+
+        Battle battle = new Battle(combatants);
+        BattleManager battleManager = new BattleManager(battle, System.in, System.out);
         battleManager.Run();
     }
 
--- src/main/java/com/turn_based_game/BattleManager.java
+++ src/main/java/com/turn_based_game/BattleManager.java
@@ -1,80 +1,66 @@
 package com.turn_based_game;
 
 import com.turn_based_game.actions.Action;
-import com.turn_based_game.actions.BasicAttack;
-import com.turn_based_game.utility.General;
+import com.turn_based_game.model.Battle;
 
-import java.util.Scanner;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class BattleManager {
-    Battle battle;
-    int currentTurn = 0; //0 = players turn, 1 = enemy turn
-    int turnCount = 0;
+    // Instance variables
+    private Battle battle;
+    private int currentTurn = 0;
+    private int turnCount = 0;
+    private InputStream in;
+    private OutputStream out;
 
+    // Constructors
+    public BattleManager(Battle battle, InputStream in, OutputStream out){
+        this.battle = battle;
+        this.in = in;
+        this.out = out;
+    }
+
+    // Behaviors
     public void Run(){
         System.out.println("\nBattle has started!\n");
 
         while(true){
             turnCount++;
             //assign current combatant
-//            Combatant currentCombatant = currentTurn == 0 ? combatantA : combatantB;
-//            Combatant otherCombatant = currentTurn == 0 ? combatantB : combatantA;
+            Combatant currentCombatant = battle.getNextCombatant();
 
             //Print Battle Status
-            //TODO: Insert printBattleStatus(); here
+            //TODO: Insert getBattleStatus(); here
+
+            //String battleStatus = String.format("%n %s %s/%s     |     %s %s/%s %n",
+//                    combatantA.getName(), combatantA.getCurrentHealth(), combatantA.getMaxHealth(),
+//                    combatantB.getName(), combatantB.getCurrentHealth(), combatantB.getMaxHealth());
+//            System.out.println(battleStatus);
 
             //prompt currentCombatant for an action
-            //TODO: Action action = currentCombatant.promptForAction()
-            //TODO: Insert printActionUsed(); here
+            Action action = currentCombatant.promptForAction();
+
+            //TODO: create something like printActionUsed(); here
 
             //run currentCombatant action
             //TODO: action.execute(currentCombatant, otherCombatant);
 
-            //Check for victory
-            if(isBattleOver()){
+            //check for victory
+            if(battle.isBattleOver()) {
+                Combatant winner = battle.getWinner();
                 break;
             }
 
-            //swap turns
-            currentTurn = currentTurn == 0 ? 1 : 0;
-
-            //this delay is only here for the CLI, needs to be moved to
-            General.delay(1);
-
-//            String battleStatus = String.format("%n %s %s/%s     |     %s %s/%s %n",
-//                    combatantA.getName(), combatantA.getCurrentHealth(), combatantA.getMaxHealth(),
-//                    combatantB.getName(), combatantB.getCurrentHealth(), combatantB.getMaxHealth());
-//            System.out.println(battleStatus);
-//
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                System.out.println("Thread was interrupted.");
-//            }
-//
-//            //check for victory
-//            int comA = combatantA.getCurrentHealth();
-//            int comB = combatantB.getCurrentHealth();
-//            if (comA <= 0 || comB <= 0) {
-//                if (comA <= 0 && comB <= 0) {
-//                    System.out.println("It's a tie, they both defeated eachother at the same time.");
-//                } else if (comB <= 0) {
-//                    System.out.println(combatantA.getName() + " Is the winner!");
-//                } else if (comA <= 0) {
-//                    System.out.println(combatantB.getName() + " Is the winner!");
-//                }
-//                break;
-//            }
-//
-//            //swap turns
-//            currentTurn = currentTurn == 0 ? 1 : 0;
+            //Rotate turn
+            currentTurn++;
+            if(currentTurn >= battle.getCombatants().size()){
+                currentTurn = 0;
+            }
         }
 
         //TODO: write out victory text
         System.out.println("\nBattle has finished!\n");
-    }
-
-    public Boolean isBattleOver(){
-        return false;
     }
 }
